d953181730e8ee3efef762289a65a71bce5fd5ad
Pedro Miranda <pbragamiranda@gmail.com>
2022-05-17 10:55:51 +0200

    coded with batch 880

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # CRUD - Create, Read, Update, Delete
+  # I can see all the restaurants
+  # get "/restaurants", to: "restaurants#index"
+  # # I can create a restaurant
+  # get "/restaurants/new", to: "restaurants#new" # display the form
+  # post "/restaurants", to: "restaurants#create"
+  # # I can edit a restaurant
+  # get "/restaurants/:id/edit", to: "restaurants#edit" # display the filled form
+  # patch "/restaurants/:id", to: "restaurants#update"
+  # # I can delete a restaurant
+  # delete "/restaurants/:id", to: "restaurants#destroy"
+  # # I can see one resturant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  resources :restaurants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: [1,2,3,4,5].sample
+  )
+end
